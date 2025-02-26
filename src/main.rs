338b5c2fb1cfad1a82d84d895c1f0fd2528b0007
Ivan Kachalkin <xikxp1@gmail.com>
2025-02-26 14:27:50 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,25 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        println!("{}: command not found", input.trim())
+        let input_string = input.trim();
+        let tokens = input_string.split_whitespace().collect::<Vec<&str>>();
+        if tokens.is_empty() {
+            continue;
+        }
+        let command = tokens[0];
+        let args = &tokens[1..];
+        match command {
+            "exit" => {
+                let exit_code = if !args.is_empty() {
+                    args[0].parse::<i32>().unwrap_or(0)
+                } else {
+                    0
+                };
+                std::process::exit(exit_code);
+            }
+            _ => {
+                println!("{}: command not found", command);
+            }
+        }
     }
 }
